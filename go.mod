081c56d06ebb9033c884de245b30b4e76e62ab17
Mark <mgaza@texasfile.com>
2022-01-10 14:28:14 -0600

    initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module Harrison-Legal-Parser
+
+go 1.17
+
+require rsc.io/quote v1.5.2
+
+require (
+	github.com/mgaza/goTools v0.0.0-20220110163230-6cd56f8e6bbf // indirect
+	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
+	rsc.io/sampler v1.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"rsc.io/quote"
+	// "github.com/mgaza/goTools"
+)
+
+func main() {
+	fmt.Println(quote.Go())
+	// Open the file
+	ericexportfile, err := os.Open("C:\\Users\\mgaza\\Desktop\\temp\\Harrison\\1903-1929\\harrison_1903-01-01_1903-12-31.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// goTools.checkError("Found an error: ", err)
+
+	// Parse the file
+	r := csv.NewReader(bufio.NewReader(ericexportfile))
+
+	// Iterate through the records
+	for {
+		// Read each record from csv
+		record, err := r.Read()
+
+		if err == io.EOF {
+			break
+		}
+		// goTools.checkError("Found an error: ", err)
+		fmt.Printf("remark: %s\n", record[13])
+	}
+}
+
+// func checkError(message string, err error) {
+// 	if err != nil {
+// 		log.Fatal(message, err)
+// 	}
+// }
